18f59e5087b24bc53ed5cdea8f96ceb17182a47e
nikneem <ekeilholz+git@outlook.com>
2025-03-25 17:40:57 +0100

    Building some

---
+++ src/HexMaster.NeuraLingo.Core/Project/DomainModels/Project.cs
@@ -0,0 +1,10 @@
+﻿namespace HexMaster.NeuraLingo.Core.Project.DomainModels;
+
+public class Project
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<string> SupportedLanguages { get; set; }
+    public string DefaultLanguage { get; set; }
+}
---
+++ src/HexMaster.NeuraLingo/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Win32;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HexMaster.NeuraLingo;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+    }
+    private static bool IsLightTheme()
+    {
+        using var key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize");
+        var value = key?.GetValue("AppsUseLightTheme");
+        return value is int i && i > 0;
+    }
+
+    [DllImport("dwmapi.dll")]
+    private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+
+    private const int DWMWA_USE_IMMERSIVE_DARK_MODE_BEFORE_20H1 = 19;
+    private const int DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
+
+    private static bool UseImmersiveDarkMode(IntPtr handle, bool enabled)
+    {
+        if (OperatingSystem.IsWindowsVersionAtLeast(10, 0, 17763))
+        {
+            var attribute = DWMWA_USE_IMMERSIVE_DARK_MODE_BEFORE_20H1;
+            if (OperatingSystem.IsWindowsVersionAtLeast(10, 0, 18985))
+            {
+                attribute = DWMWA_USE_IMMERSIVE_DARK_MODE;
+            }
+
+            int useImmersiveDarkMode = enabled ? 1 : 0;
+            return DwmSetWindowAttribute(handle, attribute, ref useImmersiveDarkMode, sizeof(int)) == 0;
+        }
+
+        return false;
+    }
+
+    protected override void OnSourceInitialized(EventArgs e)
+    {
+        base.OnSourceInitialized(e);
+        var isLightTheme = IsLightTheme();
+        HwndSource source = (HwndSource)PresentationSource.FromVisual(this);
+        UseImmersiveDarkMode(source.Handle, !isLightTheme);
+    }
+}
